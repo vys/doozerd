1a488cf29a73be6b0d4303e99e8c250b5c2ee39a
Aram Hăvărneanu <aram@mgk.ro>
2012-03-01 19:18:17 +0100

    persistence: documentation fixes.

--- log.go
+++ log.go
@@ -1,7 +1,6 @@
-// Package log implements a persistence layer for doozer using a circular log.
-// Writes containing mutations are issued at the end of the log.  Deletions
-// are garbage collected from the head of the log so that the backing file
-// does not grow indefinetly.
+// Package log provides persistence for doozer using a circular log.
+// Doozer mutations are apended to the log.  Deletions are garbage collected
+// from the head of the log so the backing file does not grow indefinetly.
 
 package log
 
@@ -10,17 +9,19 @@ import (
 	"os"
 )
 
+// Logger implements a journal using a file on disk. Doozer logs mutations
+// to it to save state across server restarts.
 type Logger struct {
 	rws io.ReadWriteSeeker
 }
 
-// Function New opens the named log file for synchronous I/O, creating it with
-// mode 0640 if it does not exist and prepares it for logging operation.
-// If successful methods on the returned logger can be used for I/O.  It returns
-// a Logger, and an error, if any.
-//
-// BUGS: The Logger is connected directly to the file, and deleted records are
-// not garbage collected, the arena layer is not implemented yet.
+// BUG(aram): The Logger is connected directly to the file, and deleted
+// records are not garbage collected, the arena layer is not implemented yet.
+
+// New opens the named log file for synchronous I/O, creating it with mode 0640
+// if it does not exist and prepares it for logging operation.  If successful,
+// methods on the returned logger can be used for I/O.  It returns a Logger
+// and an error, if any.
 func New(name string) (l *Logger, err error) {
 	// File is created if it does not exist, file must be opened synchronously
 	// in order to guarantee consistency, file is group readable in order
